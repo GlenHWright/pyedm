f11207fcc586bb07e25c7d11f9d37200f63c677a
Glen Wright <Glen.Wright@lightsource.ca>
2022-02-18 16:47:01 -0600

    removed tabs!

--- edm.py
+++ edm.py
@@ -2,7 +2,7 @@ from  pyedm import pyedm_main
 import sys
 
 if __name__ == "__main__":
-	del sys.argv[0]
+    del sys.argv[0]
     if len(sys.argv) == 0:
         sys.argv = [ "-h" ]
-	pyedm_main(sys.argv)
+    pyedm_main(sys.argv)
