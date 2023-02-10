d7fcb088ee8589724bfec4e9c6cec9510daa81d8
Glen Wright <glen.wright@lightsource.ca>
2023-02-10 14:04:43 -0600

    Add deployment directory for potential install to pypi

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ setup(name="pyedm", version="2.1.0",
 	package_data={'pyedm' : ['modules/*.py']},
 	author="Glen Wright",
 	author_email="Glen.Wright@lightsource.ca",
-	url="http://www.lightsource.ca")
+	url="http://www.lightsource.ca",
+        install_requires=[
+            "<pyepics>",
+            "<PyQt5>",
+            "<numpy>",
+            "<pyqtgraph>"
+            ]
+        )
 
 setup(name="edm", version="2.1.0", py_modules=['edm',],)
